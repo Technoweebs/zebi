d9492e5c5a869c1efad5ea8b005c15ed1707729b
TomaruDev <tomaru.development@gmail.com>
2021-01-13 18:45:34 +0100

    Base app.js now with COMMENTS :O

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
+// Modules
 const Discord = require("discord.js");
 const client  = new Discord.Client();
 const fs      = require("fs");
 
+// Variables
 client.config   = require("./config.json");
 client.commands =
 client.events   =
 client.modules  = {};
 
-fs.readdirSync(`${__dirname}/events/`).filter((file) => /\.js$/.test(file)).forEach((file) => {
-	let event = require(`${__dirname}/events/${file}`);
-	event.init(client);
-	client.on(event.name, event.callback);
+// Commands
+fs.readdirSync(`${__dirname}/commands/`).filter((file) => /\.js$/.test(file))
+.forEach((file) => {
+	// Do some tests and register the command
 });
 
-fs.readdirSync(`${__dirname}/commands/`).filter((file) => /\.js$/.test(file)).forEach((file) => {
-	let command = require(`${__dirname}/commands/${file}`);
-	command.init(client);
-	client.commands[command.name] = command;
+// Events
+fs.readdirSync(`${__dirname}/events/`).filter((file) => /\.js$/.test(file))
+.forEach((file) => {
+	// Do some tests and register the event
 });
 
-fs.readdirSync(`${__dirname}/modules/`).filter((file) => /\.js$/.test(file)).forEach((file) => {
-	if(!client.config[file.split('.')[0]].enabled) return;
-	client.modules[file.split('.')[0]] = new (require(`${__dirname}/modules/${file}`))(client, client.config[file.split('.')[0]]);
+// Modules
+fs.readdirSync(`${__dirname}/modules/`).filter((file) => /\.js$/.test(file))
+.forEach((file) => {
+	// Do some tests and register the module
 });
 
 client.login(client.config.token);
