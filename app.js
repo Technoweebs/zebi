b4466b758133f2e83d4e1f2df20a86f18a0125c0
TomaruDev <tomaru.development@gmail.com>
2021-01-13 19:10:22 +0100

    Add command manager

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ client.modules  = {};
 // Commands
 fs.readdirSync(`${__dirname}/commands/`).filter((file) => /\.js$/.test(file))
 .forEach((file) => {
-	// Do some tests and register the command
+	let command = require(`${__dirname}/commands/${file}`);
+	if(!command.config.enabled) return;
+
+	client.commands[command.config.name] = command;
 });
 
 // Events
