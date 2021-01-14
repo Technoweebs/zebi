1824cebe8c1286b9a6bc7d087e9eb02ebf9cb66e
TomaruDev <tomaru.development@gmail.com>
2021-01-14 18:21:08 +0100

    Make the message event more lisible

--- events/message.js
+++ events/message.js
@@ -11,12 +11,9 @@ module.exports = {
 
 		message.parsed = {
 			prefix: this.config.bot.prefix,
-			args: [],
-			command: ''
+			get args() { return message.content.slice(this.prefix.length).split(' '); },
+			get command() { return this.args[0]; }
 		};
-
-		message.parsed.args = message.content.slice(message.parsed.prefix.length).split(' ');
-		message.parsed.command = message.parsed.args[0];
 
 		if(!this.client.commands[message.parsed.command]) return message.reply("That command doesn't exists.");
 		this.client.commands[message.parsed.command].exec(message);
